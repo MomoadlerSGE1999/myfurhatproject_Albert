430f88725c047dc8ee1b8b84f3d04763197af221
Moritz <moritz.vincent.hensel@w.thm.de>
2022-06-14 12:48:36 +0200

    taxi driver dialogue update

---
+++ Demo02/src/main/kotlin/furhatos/app/demo02/flow/main/Taxidriverdialogue01.kt
@@ -0,0 +1,25 @@
+package furhatos.app.demo02.flow.main
+
+import furhatos.flow.kotlin.State
+import furhatos.flow.kotlin.furhat
+import furhatos.flow.kotlin.onResponse
+import furhatos.flow.kotlin.state
+import furhatos.gestures.Gestures
+import furhatos.nlu.common.No
+import furhatos.nlu.common.Yes
+
+val Taxidriverdialogue01 : State = state() {
+
+    onEntry {
+        furhat.ask("Is that name correct?")
+        onResponse<Yes> {
+            furhat.say("Perfect, I will figure out where you have to pick up your client")
+        }
+        onResponse<No> {
+            furhat.say("Then please enter the name of your client again")
+        }
+
+
+    }
+    }
+}
--- Demo02/src/main/kotlin/furhatos/app/demo02/flow/main/taxidriverdialogue.kt
+++ Demo02/src/main/kotlin/furhatos/app/demo02/flow/main/taxidriverdialogue.kt
@@ -14,11 +14,13 @@ val Taxidriverdialogue : State = state() {
         )}
     onResponse<Yes> {
 
-        furhat.say("Please type in the sur name of your client"
+        furhat.say(
+            "Please type in the sur name of your client"
         )
         //furhat.attend(locationb) // Timer einfügen, da er nur für eine gewisse Zeit zur Tastatur schauen soll
         val userInputclientvor = readLine()
-        furhat.say("Please also type in your clients last name"
+        furhat.say(
+            "Please also type in your clients last name"
         )
         val userInputclientnach = readLine()
         fun getName(vorname: String? = userInputclientvor, name: String? = userInputclientnach): String =
@@ -28,19 +30,12 @@ val Taxidriverdialogue : State = state() {
         println("Your clients name is $vollername")
 
 
-        furhat.ask("So your clients name is: $vollername?")
 
-        onResponse<Yes> {
-        furhat.say("Perfect, I will figure out where you have to pick up your client")
-        }
-        onResponse<No> {
-        furhat.say("Then please enter the name of your client again")
-        }}
-
-    onResponse<No> {
-        furhat.say("Okay then please Sign up at our reception. It was a pleasure trying to help you!")
-        furhat.gesture(Gestures.BigSmile(1.0, 2.0))
+        furhat.say("So your clients name is: $vollername?")
+        goto(Taxidriverdialogue01)
     }
-}
+   }
+
+
 
 
