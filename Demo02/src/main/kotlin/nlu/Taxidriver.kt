a9024b5a4a300a13132fec5104afcf517446a3a5
Moritz <moritz.vincent.hensel@w.thm.de>
2022-07-28 12:36:04 +0200

    Ausklammern von vorerst nicht relevantem Code

--- Demo02/src/main/kotlin/nlu/Taxidriver.kt
+++ Demo02/src/main/kotlin/nlu/Taxidriver.kt
@@ -1,11 +1,12 @@
 import furhatos.nlu.Intent
 
 class Taxidriver : Intent() {
-    var name: String = ""
-    var vorname: String = ""
+    //var name: String = ""
+    //var vorname: String = ""
     override fun getExamples(lang: furhatos.util.Language): List<String> {
         return listOf("Taxi driver", "Taxi", "Driver", "Taxifahrer")
     }
-    fun getFullName(): String =
+    /*fun getFullName(): String =
         "$vorname $name"
+*/
 }
