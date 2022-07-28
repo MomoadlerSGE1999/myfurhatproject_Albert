5a3979cf9fb23eba234aa6fae2d2bbff578ea94d
Moritz <moritz.vincent.hensel@w.thm.de>
2022-07-28 12:36:52 +0200

    Intent Patient nun mächtiger

--- Demo02/src/main/kotlin/nlu/Patient.kt
+++ Demo02/src/main/kotlin/nlu/Patient.kt
@@ -4,7 +4,7 @@ class Patient : Intent() {
     //var name: String = ""
     //var vorname: String = ""
     override fun getExamples(lang: furhatos.util.Language): List<String> {
-        return listOf("Patient", "Client", "Inpatient", "the end", "patent", "patsient", "Patzient", "Pate", "pup tent", "Patsy end", "send", "Haitian", "partying", "Passion","habt ihr einen")
+        return listOf("Dialysepatient", "Patient", "Client", "Inpatient", "the end", "patent", "patsient", "Patzient", "Pate", "pup tent", "Patsy end", "send", "Haitian", "partying", "Passion","habt ihr einen")
     }
 }
 
