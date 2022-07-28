175f575b93dc9db5719f501bb205ee8988e66ee4
Moritz <moritz.vincent.hensel@w.thm.de>
2022-07-28 12:37:04 +0200

    Intent Ja definiert

---
+++ Demo02/src/main/kotlin/nlu/Ja.kt
@@ -0,0 +1,8 @@
+import furhatos.nlu.Intent
+
+class Ja : Intent() {
+
+    override fun getExamples(lang: furhatos.util.Language): List<String> {
+        return listOf("Ja", "Genau", "Richtig", "Korrekt", "So ist es", "Das ist richtig", "Ja, korrekt", "Yes", "Auf jeden Fall", "Ganz Genau", "Ja, so ist es", "Genau richtig", "ja, bin ich", "ja, das ist wahr", "Jap","habt ihr einen")
+    }
+}
