85d6ee866c5b57b384686f8dc939694d38648de1
Moritz <moritz.vincent.hensel@w.thm.de>
2022-08-16 14:41:53 +0200

    Um mehr Beispiele ergänzt damit Intent besser und robuster erkannt wird

--- Demo02/src/main/kotlin/nlu/Patient.kt
+++ Demo02/src/main/kotlin/nlu/Patient.kt
@@ -4,7 +4,9 @@ class Patient : Intent() {
     //var name: String = ""
     //var vorname: String = ""
     override fun getExamples(lang: furhatos.util.Language): List<String> {
-        return listOf("Dialysepatient", "Patient", "Client", "Inpatient", "the end", "patent", "patsient", "Patzient", "Pate", "pup tent", "Patsy end", "send", "Haitian", "partying", "Passion","habt ihr einen")
+        return listOf("habt ihr", "hat Jan", "Jens", "impatient", "Bernd", "Dialysepatient", "Patient", "Client", "Inpatient", "the end", "patent", "patsient", "Patzient", "Pate", "pup tent", "Patsy end", "send", "Haitian", "partying", "Passion","habt ihr einen")
     }
 }
+
+
 
