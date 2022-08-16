2f430b4a1dd5046f52279e9253df9fade658b75e
Moritz <moritz.vincent.hensel@w.thm.de>
2022-08-16 11:35:56 +0200

    dialog logger hinzugefügt

--- Demo02/src/main/kotlin/furhatos/app/demo02/flow/init.kt
+++ Demo02/src/main/kotlin/furhatos/app/demo02/flow/init.kt
@@ -1,21 +1,27 @@
 package furhatos.app.demo02.flow
 
 
+import NummerErfragen
+import furhatos.app.demo02.flow.main.Greeting
 import furhatos.app.demo02.setting.distanceToEngage
 import furhatos.app.demo02.setting.maxNumberOfUsers
 import furhatos.app.demo02vergleich.flow.main.Idle
 import furhatos.flow.kotlin.*
+import furhatos.flow.kotlin.voice.AcapelaVoice
+import furhatos.flow.kotlin.voice.PollyNeuralVoice
 import furhatos.flow.kotlin.voice.Voice
 import furhatos.util.Gender
 import furhatos.util.Language
 
 val Init : State = state() {
     init {
-        furhat.setVoice(Language.GERMAN, Gender.FEMALE, false)
-        furhat.setInputLanguage(Language.GERMAN)
+        furhat.run {
+            dialogLogger.startSession()
+            setVoice(Language.GERMAN, Gender.FEMALE, false)
+            setInputLanguage(Language.GERMAN)
+        }
         /** Set our default interaction parameters */
         users.setSimpleEngagementPolicy(distanceToEngage, maxNumberOfUsers)
-        furhat.voice = Voice("Hannah")
         /** start the interaction */
         goto(Idle)
     }
