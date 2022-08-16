951d6034933e1f94465ba08b494ad363bd53f7f9
Moritz <moritz.vincent.hensel@w.thm.de>
2022-08-16 11:34:58 +0200

    automatische head movements, aktuelleres user handling aus beispielskill von furhat robotics

--- Demo02/src/main/kotlin/furhatos/app/demo02/flow/main/idle.kt
+++ Demo02/src/main/kotlin/furhatos/app/demo02/flow/main/idle.kt
@@ -1,5 +1,6 @@
 package furhatos.app.demo02vergleich.flow.main
 
+import furhat.libraries.standard.AutomaticHeadMovements
 import furhat.libraries.standard.utils.attendClosestUser
 import furhatos.app.demo02.flow.main.Greeting
 import furhatos.app.demo02.flow.main.user
@@ -7,23 +8,24 @@ import furhatos.flow.kotlin.*
 
 val Idle: State = state {
 
-    init {
-        when {
-            users.count > 0 -> {
-                furhat.attend(users.random)
-                goto(Greeting)
-            }
-            users.count == 0 && furhat.isVirtual() -> furhat.say("Ich kann niemanden sehen, füge bitte einen virtuellen Benutzer hinzu")
-            users.count == 0 && !furhat.isVirtual() -> furhat.say("Ich kann niemanden sehen, komm bitte näher ")
-        }
-    }
+    include(AutomaticHeadMovements.RandomHeadMovements(repetitionPeriod = 5000..10000))
 
     onEntry {
-        furhat.attendClosestUser()
-    }
+        if (users.count == 1) {
+            goto(Greeting)
+        } else if (users.count > 1) {
+            furhat.attendClosestUser()
+            //Furhat schaut bei einer Gruppe bestehend aus mehr als zwei Usern den von ihm aus gesehen nächsten user an
+            goto(Greeting)
+        } else {
+            furhat.attendNobody()
+            /** Set value to avoid AutomaticHeadMovements to interfere with other head movements defined in a gesture. We assume gestures are not longer than 3000 ms **/
+            AutomaticHeadMovements.autoHeadMovementDelay(3000)
 
-    onUserEnter {
-        furhat.attend(it)
-        goto(Greeting)
+            onUserEnter {
+                furhat.attend(it)
+                goto(Greeting)
+            }
+        }
     }
 }
