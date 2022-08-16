1dc7f068accf46f6bfc625a09d55ddcf8cc4bc5e
Moritz <moritz.vincent.hensel@w.thm.de>
2022-08-16 14:44:00 +0200

    Mehr Beispiele

--- Demo02/src/main/kotlin/nlu/Taxidriver.kt
+++ Demo02/src/main/kotlin/nlu/Taxidriver.kt
@@ -4,7 +4,7 @@ class Taxidriver : Intent() {
     //var name: String = ""
     //var vorname: String = ""
     override fun getExamples(lang: furhatos.util.Language): List<String> {
-        return listOf("Taxi driver", "Taxi", "Driver", "Taxifahrer")
+        return listOf("Taxi driver", "Taxi", "Driver", "Taxifahrer", "Fahrer", "ich bin Taxifahrer", "Ich bin Cara")
     }
     /*fun getFullName(): String =
         "$vorname $name"
