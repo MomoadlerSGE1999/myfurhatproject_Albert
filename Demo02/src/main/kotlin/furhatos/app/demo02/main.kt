56c6a73408f99eeb1d610fe2e60512c45c89eb81
Moritz <moritz.vincent.hensel@w.thm.de>
2022-10-15 12:54:20 +0200

    Bereit fürs Testing

--- Demo02/src/main/kotlin/furhatos/app/demo02/main.kt
+++ Demo02/src/main/kotlin/furhatos/app/demo02/main.kt
@@ -7,12 +7,20 @@ import furhatos.skills.Skill
 
 class Demo02Skill : Skill() {
     override fun start() {
-        //Hier gibt man den State an in dem die interaktion starten soll, typischerweise ist das der State Init
+        //Hier gibt man den State an in dem die interaktion starten soll, typischerweise ist das der State Init.
         Flow().run(Init)
     }
 }
-//In Zeile 15 wird der Skill gestartet
+//In Zeile 15 wird der Skill gestartet.
 fun main(args: Array<String>) {
     Skill.main(args)
 }
+
+
+
+
+
+
+
+
 
